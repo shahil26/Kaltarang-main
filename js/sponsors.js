a202c94f6a45857c18fd9c14aa0fb9c2deca2b22
ROHIT KUMAR <112969162+code-hamster-rohit@users.noreply.github.com>
2024-03-06 05:54:03 +0530

    Add files via upload

---
+++ js/sponsors.js
@@ -0,0 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const columns = ["#column1", "#column2", "#column3", "#column4", "#column5", "#column6", "#column7"];
+
+columns.forEach((column, index) => {
+    const direction = index % 2 === 1 ? -1 : 1;
+    const xOffset = window.innerWidth < 1000 ? direction * 1000 : direction * 1000;
+
+    gsap.from(`${column} div`, {
+        scrollTrigger: {
+            trigger: `${column} div`,
+            start: "top bottom",
+            end: "bottom 80%",
+            scrub: window.innerWidth < 1000 ? 1 : 0.5,
+            markers: false,
+            toggleActions: "restart pause reverse pause",
+        },
+        x: xOffset,
+        opacity: 0,
+        duration: 0.1,
+        ease: "power1.inOut",
+    });
+});
