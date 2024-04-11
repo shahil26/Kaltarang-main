2d6afadcd430bd325737ce7e88ad6983dd40c9c2
ROHIT KUMAR <112969162+code-hamster-rohit@users.noreply.github.com>
2024-04-12 01:31:48 +0530

    Add files via upload

--- js/loader.js
+++ js/loader.js
@@ -5,7 +5,7 @@ window.addEventListener('load', (event) => {
     let currentDate = new Date();
   
     // Target future date/24 hour time/Timezone
-    let targetDate = moment.tz("2024-04-12 00:00", "Asia/Kolkata");
+    let targetDate = moment.tz("2024-04-14 23:59", "Asia/Kolkata");
   
     // Calculate the difference in seconds between the future and current date
     let diff = targetDate / 1000 - currentDate.getTime() / 1000;
